5aa49421ccf76c596a8f6cf3c37f439c166ea5c9
Davis Zalitis <davis.zalitis@whitedigital.eu>
2023-02-01 17:38:47 +0200

    chore: create fixture

---
+++ src/DataFixture/StorageItemFixture.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types = 1);
+
+namespace WhiteDigital\ApiResource\DataFixture;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use WhiteDigital\ApiResource\Entity\StorageItem;
+
+use function copy;
+
+class StorageItemFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $templateFileName = __DIR__ . '/assets/storage_item-fixture.template.txt';
+        for ($i = 0; $i < 2; $i++) {
+            $fileName = __DIR__ . '/assets/storage_iten-' . $i . '.txt';
+
+            copy($templateFileName, $fileName);
+            $file = new UploadedFile($fileName, 'storage_item-' . $i . '.txt', 'text/plain', test: true);
+
+            $fixture = (new StorageItem())->setFile($file);
+
+            $manager->persist($fixture);
+            $manager->flush();
+
+            $this->addReference('wdFile' . $i, $fixture);
+        }
+    }
+}
